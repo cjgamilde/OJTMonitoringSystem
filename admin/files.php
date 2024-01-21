f916d93425a62a5240f183c9cac250fa5b238133
cjgamilde <cjgamilde@gmail.com>
2024-01-22 00:39:21 +0800

    filemodule design admin

--- admin/files.php
+++ admin/files.php
@@ -58,52 +58,52 @@ function formatBytes($file_bytes, $precision = 2){
 
 
    <div class="container-fluid">
-    <div class="row" >
+    <div class="row">
         <div class="col-sm-auto bg-white sticky-top shadow">
-            <div class=" d-flex flex-sm-column flex-row flex-nowrap bg-white align-items-center sticky-top">
+            <div class="d-flex flex-sm-column flex-row flex-nowrap bg-white align-items-center sticky-top">
                 <a href="index.php" class="d-block p-3 link-dark text-decoration-none" title="" data-bs-toggle="tooltip" data-bs-placement="right" data-bs-original-title="Icon-only">
                    <img src="../src/images/ntclogo.PNG" class="img-fluid" alt="...">
                 </a>
-                        <h3>Student Portal</h3>
-                        <br>
-                <ul class="nav nav-pills nav-flush flex-sm-column flex-row flex-nowrap mb-auto mx-auto justify-content-between w-100 px-3" style = "align-items:start; text-align:left;">
-                    <li class="nav-item">
-                       <a href="index.php" class="nav-link " title="" data-bs-toggle="tooltip" data-bs-placement="right" data-bs-original-title="Dashboard" name="dashboard">
-                           <i class="bi bi-house-fill fs-5"></i> DashBoard
-                        </a>
-                    </li>
-                    <li>
-                        <a href="files.php" class="nav-link " title="" data-bs-toggle="tooltip" data-bs-placement="right" data-bs-original-title="Student List">
-                         <i class="bi bi-folder-fill fs-5"></i> Files
-                        </a>
-                    </li>
-                    <li>
-                        <a href="attendance.php" class="nav-link" title="" data-bs-toggle="tooltip" data-bs-placement="right" data-bs-original-title="Announcement">
-                           <i class="bi bi-clock-fill fs-5"></i> Attendance
-                        </a>
-                    </li>
-                    <li>
-                        <a href="activity.php" class="nav-link" title="" data-bs-toggle="tooltip" data-bs-placement="right" data-bs-original-title="Announcement">
-                           <i class="bi bi-list-ul fs-5"></i> Activity
-                        </a>
-                    </li>
+                <h3> Admin Portal</h3>
+                            <br>
+                    <ul class="nav nav-pills nav-flush flex-sm-column flex-row flex-nowrap mb-auto mx-auto justify-content-between w-100 px-3" style = "align-items:start; text-align:left;">
+                        <li class="nav-item">
+                            <a href="index.php" class="nav-link " title="" data-bs-toggle="tooltip" data-bs-placement="right" data-bs-original-title="Dashboard" name="dashboard">
+                            <i class="bi bi-house-fill fs-3"></i> DashBoard
+                            </a>
+                        </li>
+                        <li class="nav-item">
+                            <a href="student_list.php" class="nav-link" title="" data-bs-toggle="tooltip" data-bs-placement="right" data-bs-original-title="Student List" name="studentlist">
+                            <i class="bi bi-person-lines-fill fs-3"></i> Student List
+                            </a>
+                        </li>
+                        <li class="nav-item">
+                            <a href="announcement.php" class="nav-link" title="" data-bs-toggle="tooltip" data-bs-placement="right" data-bs-original-title="Announcement" name="announcement">
+                            <i class="bi bi-megaphone-fill fs-3"></i> Announcement
+                            </a>
+                        </li>
+                        <li class="nav-item">
+                            <a href="register.php" class="nav-link" title="" data-bs-toggle="tooltip" data-bs-placement="right" data-bs-original-title="Announcement" name="register">
+                            <i class="bi bi-person-fill-add fs-3"></i> Register
+                            </a>
+                        </li>
 
-                    <li class="nav-item">
-                        <a href="settings.php" class="nav-link" title="" data-bs-toggle="tooltip" data-bs-placement="right" data-bs-original-title="Announcement" >
-                           <i class="bi bi-gear-fill fs-5"></i> Settings
-                        </a>
-                    </li>
-                    <li>
-                        <hr>
-                        <a href="../include/logout.php" class="nav-link" title="" data-bs-toggle="tooltip" data-bs-placement="right" data-bs-original-title="Announcement">
-                           <i class="bi bi-box-arrow-left fs-5" style = "padding-right:10px; "></i>Logout
-                        </a>
-                    </li>
-                </ul>
+                        <li class="nav-item">
+                            <a href="settings.php" class="nav-link" title="" data-bs-toggle="tooltip" data-bs-placement="right" data-bs-original-title="Announcement" name="settings">
+                            <i class="bi bi-gear-fill fs-3"></i> Settings
+                            </a>
+                        </li>
+                            <br>
+                        <li>
+                            <hr>
+                            <a href="../include/logout.php" class="nav-link" title="" data-bs-toggle="tooltip" data-bs-placement="right" data-bs-original-title="Announcement">
+                            <i class="bi bi-box-arrow-left fs-55" style = "padding-right:10px; "></i>Logout
+                            </a>
+                        </li>
+                    </ul>
+                </div>
             </div>
-        </div>
-
-    <div class="col-sm p-3 min-vh-100">
+        <div class="col-sm p-3 min-vh-100">
          <div class="container-xxl">
             <h1>PROFILE</h1>
             <hr> 
